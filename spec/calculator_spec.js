92e30d3199ac33a78320ee8b5add3335239709e9
ritabc <ritabc@gmail.com>
2018-08-04 11:58:09 -0700

    Complete work on average life expectancy calculator

--- spec/calculator_spec.js
+++ spec/calculator_spec.js
@@ -4,31 +4,39 @@ import { Birthday } from './../src/birthday.js';
 
 describe('Calculator', function() {
 var reusableDob;
+var calculator;
 
   beforeEach(function() {
     reusableDob = new Birthday(1983, 5, 16)
+    calculator = new Calculator()
   });
 
   it('will return the age of a human in Mercury years', function() {
-    let calculator = new Calculator()
-    let mercuryYears = calculator.getMercuryYears(reusableDob.ageInSeconds());
+    let mercuryYears = calculator.secondsToMercuryYears(reusableDob.ageInSeconds());
     expect(Math.round(mercuryYears)).toEqual(Math.round(reusableDob.ageInSeconds() / (31557600 * 0.24)));
   })
 
   it('will return the age of a human in Venus years', function() {
-    let calculator = new Calculator()
-    let venusYears = calculator.getVenusYears(reusableDob.ageInSeconds());
+    let venusYears = calculator.secondsToVenusYears(reusableDob.ageInSeconds());
     expect(Math.round(venusYears)).toEqual(Math.round(reusableDob.ageInSeconds() / (31557600 * 0.62)));
   })
 
   it('will return the age of a human in Mars years', function() {
-    let calculator = new Calculator()
-    let marsYears = calculator.getMarsYears(reusableDob.ageInSeconds());
+    let marsYears = calculator.secondsToMarsYears(reusableDob.ageInSeconds());
     expect(Math.round(marsYears)).toEqual(Math.round(reusableDob.ageInSeconds() / (31557600 * 1.88)));
   })
   it('will return the age of a human in Jupiter years', function() {
-    let calculator = new Calculator()
-    let jupiterYears = calculator.getJupiterYears(reusableDob.ageInSeconds());
+    let jupiterYears = calculator.secondsToJupiterYears(reusableDob.ageInSeconds());
     expect(Math.round(jupiterYears)).toEqual(Math.round(reusableDob.ageInSeconds() / (31557600 * 11.86)));
   })
+
+  it('will take the average life expectancy from the user in Earth years and more recent dob and return their remaining expected years, or the number of years past that', function() {
+    expect(calculator.aboveBelowAverageLE(reusableDob, 75)).toEqual(("With the inputted average life expectancy of 75 Earth Years, your remaining time is expected to be: 166 Mercury Years, 64 Venus Years, 21 Mars Years, or 3 Jupiter Years!"))
+  })
+
+  it('will take the average life expectancy from the user in Earth years and an older dob and return their remaining expected years, or the number of years past that', function() {
+    let oldPersonDob = new Birthday(1930, 6, 1)
+    expect(calculator.aboveBelowAverageLE(oldPersonDob, 75)).toEqual(("Congratulations, you have lived past the the inputted average life expectancy of 75 Earth Years by 55 Mercury Years, 21 Venus Years, 7 Mars Years, or 1 Jupiter Years!"))
+  })
+
 })
--- src/calculator.js
+++ src/calculator.js
@@ -1,6 +1,4 @@
 class Calculator {
-
-
   constructor() {
     const year1 = new Date(2017, 1, 1);
     const year2 = new Date(2018, 1, 1);
@@ -11,20 +9,40 @@ class Calculator {
     this.secondsInJupiterYear = this.secondsInEarthYear * 11.86;
   }
 
-  getMercuryYears(secondsOld) {
-    return (secondsOld/this.secondsInMercuryYear);
+  secondsToMercuryYears(seconds) {
+    return (seconds/this.secondsInMercuryYear);
   }
 
-  getVenusYears(secondsOld) {
-    return (secondsOld/this.secondsInVenusYear);
+  secondsToVenusYears(seconds) {
+    return (seconds/this.secondsInVenusYear);
   }
 
-  getMarsYears(secondsOld) {
-    return (secondsOld/this.secondsInMarsYear);
+  secondsToMarsYears(seconds) {
+    return (seconds/this.secondsInMarsYear);
   }
 
-  getJupiterYears(secondsOld) {
-    return (secondsOld/this.secondsInJupiterYear);
+  secondsToJupiterYears(seconds) {
+    return (seconds/this.secondsInJupiterYear);
+  }
+
+  aboveBelowAverageLE(dob, earthYearsExpectancy) {
+    let averageLeInSeconds = earthYearsExpectancy * this.secondsInEarthYear;
+    let ageInSeconds = dob.ageInSeconds();
+    let secondsLeft = averageLeInSeconds - ageInSeconds;
+    let outlivedExpectancy = false
+    if (secondsLeft < 0) {
+      outlivedExpectancy = true;
+    }
+    let mercuryYearsLeft = this.secondsToMercuryYears(secondsLeft);
+    let venusYearsLeft = this.secondsToVenusYears(secondsLeft);
+    let marsYearsLeft = this.secondsToMarsYears(secondsLeft);
+    let jupiterYearsLeft = this.secondsToJupiterYears(secondsLeft);
+
+    if (outlivedExpectancy) {
+      return `Congratulations, you have lived past the the inputted average life expectancy of ${earthYearsExpectancy} Earth Years by ${Math.round(Math.abs(mercuryYearsLeft))} Mercury Years, ${Math.round(Math.abs(venusYearsLeft))} Venus Years, ${Math.round(Math.abs(marsYearsLeft))} Mars Years, or ${Math.round(Math.abs(jupiterYearsLeft))} Jupiter Years!`
+    } else {
+      return `With the inputted average life expectancy of ${earthYearsExpectancy} Earth Years, your remaining time is expected to be: ${Math.round(Math.abs(mercuryYearsLeft))} Mercury Years, ${Math.round(Math.abs(venusYearsLeft))} Venus Years, ${Math.round(Math.abs(marsYearsLeft))} Mars Years, or ${Math.round(Math.abs(jupiterYearsLeft))} Jupiter Years!`
+    }
   }
 
 }
